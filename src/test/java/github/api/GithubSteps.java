1ffba0ab1bde9af9794c0fce3837c90f2d50063f
Sergey Ushatov <sushatov@deloitte.com>
2017-08-23 16:59:42 +0300

    test skeleton for GET github commits

--- src/test/java/github/api/GithubSteps.java
+++ src/test/java/github/api/GithubSteps.java
@@ -1,4 +1,28 @@
 package github.api;
 
+import model.GHCommits;
+import model.Rest;
+import org.jbehave.core.annotations.Pending;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+
 public class GithubSteps {
+
+    @When("I GET github commits for test repo")
+    public void whenIGETGithubCommitsForTestRepo() throws IOException {
+        GHCommits commits = Rest.getTestRepoCommits();
+        System.out.println("dcsdc");
+    }
+
+    @Then("I see a list of commits for the test repo")
+    public void thenISeeAListOfCommitsForTheTestRepo() {
+        // PENDING
+    }
 }
--- src/test/resources/stories.github.api/github_api.story
+++ src/test/resources/stories.github.api/github_api.story
@@ -5,7 +5,6 @@ As a user with test task
 I want to test GitHub API
 So that I complete the task
 
-Scenario: Test gitHub pull requests
-Given a system state
-When I do something
-Then system is in a different state
+Scenario: Test gitHub commits
+When I GET github commits for test repo
+Then I see a list of commits for the test repo
